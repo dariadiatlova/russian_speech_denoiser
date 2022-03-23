ed039a72129bcc2196fe2e7d4636c57e0878cd98
diatlova <dari.diatlova@gmail.com>
2022-03-23 16:39:34 +0300

    updated multiprocess script version

--- load_clean_speech/opus_to_wav.sh
+++ load_clean_speech/opus_to_wav.sh
@@ -1,20 +1,34 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# for f in /home/dadyatlova_1/dataset/open_stt/radio_opus_files/*.opus; do
+opus_files_directory=$1
+wav_files_directory=$2
+txt_files_directory=$3
+
+# write to array all files with a .opus extansion
+FILES=$(find $opus_files_directory -type f -name "*.opus")
+
+# naive implementation
+#for f in $FILES; do
 #  filename="${f##*/}"
 #  filename="${filename%.*}"
-#  ffmpeg -i "$f" "/home/dadyatlova_1/dataset/open_stt/radio_wav_files/${filename}.wav"; done
+#  ffmpeg -i "$f" "${wav_files_directory}/${filename}.wav"; done
 
 # multiprocess implementation
+
 opus_to_wav() {
 
   opus_files_directory=$1
   wav_files_directory=$2
-  f=$3
+  txt_files_directory=$3
+  f=$4
 
   filename="${f##*/}"
   filename="${filename%.*}"
+  # copies transcript into new directory
+  cp "$opus_files_directory/${filename}.txt" "$txt_files_directory/${filename}.txt"
+  # converts opus into wav
   ffmpeg -i "$f" "$wav_files_directory/${filename}.wav";
+}
 
 # make a constrain on max number of processes to use
 max_num_processes=$(ulimit -u)
@@ -22,10 +36,7 @@ limiting_factor=4
 num_processes=$((max_num_processes/limiting_factor))
 
 
-opus_files_directory=$1
-wav_files_directory=$2
-
-for f in "$opus_files_directory/*.opus"; do
+for f in $FILES; do
   ((i=i%num_processes)); ((i++==0)) && wait
-  opus_to_wav "$opus_files_directory" "$wav_files_directory" "$f" &
+  opus_to_wav "$opus_files_directory" "$wav_files_directory" "$txt_files_directory" "$f" &
   done
